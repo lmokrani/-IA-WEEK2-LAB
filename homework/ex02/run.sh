bb4c16b0ca1664b4c6dab1a69d4adc061c9a896a
Lyes Mokrani <lmokrani@WFIL281.cscc.local>
2022-09-07 16:24:05 -0400

    Solution Ex02

--- homework/ex02/run.sh
+++ homework/ex02/run.sh
@@ -1,2 +1,2 @@
 # You should replace the next line with your command to create a network
-docker help
+docker run -d --rm -v cscc-storage:/usr/local/apache2/htdocs/ --net cscc-network --name web httpd:2.4 
